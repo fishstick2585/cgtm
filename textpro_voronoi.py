1de93f632dfeb24ee0dddfe61c0381cd4e8f3c28
fishstick2585 <70384566+fishstick2585@users.noreply.github.com>
2020-10-22 10:47:07 -0400

    Update textpro_voronoi.py

--- textpro_voronoi.py
+++ textpro_voronoi.py
@@ -1,14 +1,10 @@
 import numpy as np
 import re
-# Read list of resids; split into array of lists and convert to integers
+# Read list of resids; split into array of lists and convert to integers. 
 input_file2 = open('processHop.out','r')
 full_file_contents = input_file2.read()
 rts_intermed = re.split('First shell: |\nSecond shell: |\n', full_file_contents)
-#for i in lines_of_fie:
-#    lines_of_file[i] = lines_of_file[i].split('Second shell: ')
-#for i in lines_of_file, # here yo need to split these too by 'Second shell: '
-#rts_intermed= np.array(lines_of_file)
-#num_shellana = 2;
+
 range_cuta = (len(rts_intermed) - 1)
 range_cutb = range_cuta*2
 range_cut = range_cutb/4
@@ -30,3 +26,5 @@ file = open("rts_fs.txt", "w")
 for index in range(len(rts_fs)):
     file.write(str(rts_fs[index]) + " " + str(rts_ss[index]) + "\n") # + " " + str(b[index]) + "\n")
 file.close()
+# output: list of numbers of each lipid type in first shell and second shell. Each line corresponds to one frame of simulation processed by processHop Voronoi.
+# Example: 16 5 23 28 13 17 in line 1 means 16, 5, and 23 of each lipid type in 1st shell and 28, 13, 17 in second shell in first processed frame.
